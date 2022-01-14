35d9ac9c99d7f430e0b3d1f89a16ce126decbed0
Cole Hartman <Col3@ou.edu>
2022-01-14 11:40:26 -0600

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square function
+#'
+#' takes vector and returns vector of squared compnents
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:20; myf(x)
+myf = function(x){
+  x^2
+}
